2726f58e1b4210b5fcb83b4e3d2841de0267c9e3
baticlean <baticlean225@gmail.com>
2025-07-31 03:23:11 +0000

    12.1

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,52 +2,54 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User.model.js');
+const jwt = require('jsonwebtoken'); // Importer jwt
 const { isAuthenticated } = require('../middleware/isAdmin.js');
 
-// ROUTE PUT /api/user/profile - Met à jour les infos de l'utilisateur connecté
+// Crée et renvoie un nouveau token pour un utilisateur
+const generateToken = (user) => {
+  const { _id, username, email, phoneNumber, role, status, profilePicture } = user;
+  const payload = { _id, username, email, phoneNumber, role, status, profilePicture };
+  return jwt.sign(payload, process.env.JWT_SECRET, {
+    algorithm: 'HS256',
+    expiresIn: '6h',
+  });
+};
+
 router.put('/profile', isAuthenticated, async (req, res) => {
   try {
     const userId = req.auth._id;
     const { username, email, phoneNumber } = req.body;
-
-    // On vérifie que les champs ne sont pas vides
     if (!username || !email || !phoneNumber) {
       return res.status(400).json({ message: 'Tous les champs sont requis.' });
     }
-
     const updatedUser = await User.findByIdAndUpdate(
       userId,
       { username, email, phoneNumber },
       { new: true }
     ).select('-passwordHash');
 
-    if (!updatedUser) {
-      return res.status(404).json({ message: 'Utilisateur non trouvé.' });
-    }
+    if (!updatedUser) { return res.status(404).json({ message: 'Utilisateur non trouvé.' }); }
 
-    res.status(200).json(updatedUser);
+    const authToken = generateToken(updatedUser);
+    res.status(200).json({ authToken });
   } catch (error) {
     res.status(500).json({ message: 'Erreur interne du serveur.' });
   }
 });
 
-// ROUTE PUT /api/user/profile-picture - Met à jour la photo de profil
 router.put('/profile-picture', isAuthenticated, async (req, res) => {
   try {
     const userId = req.auth._id;
-    const { profilePictureUrl } = req.body; // On attend l'URL de l'image de Cloudinary
-
+    const { profilePictureUrl } = req.body;
     const updatedUser = await User.findByIdAndUpdate(
       userId,
       { profilePicture: profilePictureUrl },
       { new: true }
     ).select('-passwordHash');
+    if (!updatedUser) { return res.status(404).json({ message: 'Utilisateur non trouvé.' }); }
 
-    if (!updatedUser) {
-      return res.status(404).json({ message: 'Utilisateur non trouvé.' });
-    }
-
-    res.status(200).json(updatedUser);
+    const authToken = generateToken(updatedUser);
+    res.status(200).json({ authToken });
   } catch (error) {
     res.status(500).json({ message: 'Erreur interne du serveur.' });
   }
