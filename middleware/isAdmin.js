3a04630f212b698b1e36744c9cfb67763c5c81d6
baticlean <baticlean225@gmail.com>
2025-08-03 10:57:04 +0000

    Bugs

--- middleware/isAdmin.js
+++ middleware/isAdmin.js
@@ -1,18 +1,23 @@
 const jwt = require('jsonwebtoken');
 
+// Ce middleware vérifie si un token est valide
 const isAuthenticated = (req, res, next) => {
   try {
+    // Le token est envoyé dans l'en-tête "Authorization: Bearer <TOKEN>"
     const token = req.headers.authorization.split(' ')[1];
-    if (!token) { return res.status(401).json({ message: 'Aucun token fourni.' }); }
-
+    if (!token) {
+      return res.status(401).json({ message: 'Aucun token fourni.' });
+    }
+    // On vérifie que le token est valide avec notre clé secrète
     const payload = jwt.verify(token, process.env.JWT_SECRET);
-    req.auth = payload;
+    req.auth = payload; // On attache les infos de l'utilisateur à la requête
     next();
   } catch (error) {
-    res.status(401).json({ message: 'Token invalide.' });
+    return res.status(401).json({ message: 'Token invalide.' });
   }
 };
 
+// Ce middleware vérifie si l'utilisateur est Admin ou Super Admin
 const isAdmin = (req, res, next) => {
   if (req.auth.role !== 'admin' && req.auth.role !== 'superAdmin') {
     return res.status(403).json({ message: 'Accès refusé. Droits administrateur requis.' });
@@ -20,6 +25,7 @@ const isAdmin = (req, res, next) => {
   next();
 };
 
+// Ce middleware vérifie si l'utilisateur est Super Admin
 const isSuperAdmin = (req, res, next) => {
   if (req.auth.role !== 'superAdmin') {
     return res.status(403).json({ message: 'Accès refusé. Droits Super Administrateur requis.' });
