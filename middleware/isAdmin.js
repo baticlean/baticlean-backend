aff6fe6e317cc37033d8f4b26b8ced10a5789393
baticlean <baticlean225@gmail.com>
2025-08-05 16:16:50 +0000

    Bugs

--- middleware/isAdmin.js
+++ middleware/isAdmin.js
@@ -3,14 +3,13 @@ const jwt = require('jsonwebtoken');
 const isAuthenticated = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
-    if (!token) {
-      return res.status(401).json({ message: 'Aucun token fourni.' });
-    }
+    if (!token) { return res.status(401).json({ message: 'Aucun token fourni.' }); }
+
     const payload = jwt.verify(token, process.env.JWT_SECRET);
     req.auth = payload;
     next();
   } catch (error) {
-    return res.status(401).json({ message: 'Token invalide.' });
+    res.status(401).json({ message: 'Token invalide.' });
   }
 };
 
