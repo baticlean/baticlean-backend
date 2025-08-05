cddf2b1b090d1a7f43e8bf23ae4e8d9d6d4f7678
baticlean <baticlean225@gmail.com>
2025-08-05 15:58:48 +0000

    Bugs

--- middleware/isAdmin.js
+++ middleware/isAdmin.js
@@ -2,14 +2,19 @@ const jwt = require('jsonwebtoken');
 
 const isAuthenticated = (req, res, next) => {
   try {
+    // Le token est envoyé dans l'en-tête "Authorization: Bearer <TOKEN>"
     const token = req.headers.authorization.split(' ')[1];
-    if (!token) { return res.status(401).json({ message: 'Aucun token fourni.' }); }
+    if (!token) {
+      return res.status(401).json({ message: 'Aucun token fourni.' });
+    }
 
+    // On vérifie que le token est valide avec notre clé secrète
     const payload = jwt.verify(token, process.env.JWT_SECRET);
-    req.auth = payload;
+    req.auth = payload; // On attache les infos de l'utilisateur à la requête
     next();
   } catch (error) {
-    res.status(401).json({ message: 'Token invalide.' });
+    // Si jwt.verify échoue (token invalide, expiré, etc.), on renvoie une erreur
+    return res.status(401).json({ message: 'Token invalide.' });
   }
 };
 
