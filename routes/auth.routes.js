a2b6cafe58e68d407bf5e09fd816d2b2ea9a5c59
baticlean <baticlean225@gmail.com>
2025-08-06 19:39:20 +0000

    24

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -17,11 +17,7 @@ router.post('/register', async (req, res) => {
     const salt = await bcrypt.genSalt(12);
     const passwordHash = await bcrypt.hash(password, salt);
     await User.create({ username, email, passwordHash, phoneNumber });
-
-    // --- NOTIFICATION TEMPS RÉEL AJOUTÉE ICI ---
-    // On notifie les admins qu'un nouvel utilisateur s'est inscrit
     req.io.emit('userListUpdated');
-
     res.status(201).json({ message: `Utilisateur créé avec succès !` });
   } catch (error) {
     res.status(500).json({ message: 'Erreur interne du serveur.' });
@@ -36,22 +32,28 @@ router.post('/login', async (req, res) => {
     const user = await User.findOne({ $or: [{ email: login }, { phoneNumber: login }] });
     if (!user) { return res.status(401).json({ message: 'Identifiant ou mot de passe incorrect.' }); }
 
-    const isPasswordCorrect = await bcrypt.compare(password, user.passwordHash);
-    if (!isPasswordCorrect) { return res.status(401).json({ message: 'Identifiant ou mot de passe incorrect.' }); }
-
-    const { _id, username, role, email, status, profilePicture } = user;
-    const payload = { _id, email, username, role, status, profilePicture };
-    const authToken = jwt.sign(payload, process.env.JWT_SECRET, {
-      algorithm: 'HS256',
-      expiresIn: '6h',
-    });
-
     if (user.status !== 'active') {
+        const { _id, username, role, email, status, profilePicture, phoneNumber } = user;
+        const payload = { _id, email, username, role, status, profilePicture, phoneNumber };
+        const authToken = jwt.sign(payload, process.env.JWT_SECRET, { algorithm: 'HS256', expiresIn: '6h' });
         return res.status(403).json({ 
             message: 'Votre compte a été suspendu ou banni.',
             authToken: authToken
         });
     }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.passwordHash);
+    if (!isPasswordCorrect) { return res.status(401).json({ message: 'Identifiant ou mot de passe incorrect.' }); }
+
+    // --- LA CORRECTION EST ICI ---
+    // On s'assure d'inclure 'phoneNumber' dans le token
+    const { _id, username, role, email, status, profilePicture, phoneNumber } = user;
+    const payload = { _id, email, username, role, status, profilePicture, phoneNumber };
+
+    const authToken = jwt.sign(payload, process.env.JWT_SECRET, {
+      algorithm: 'HS256',
+      expiresIn: '6h',
+    });
 
     res.status(200).json({ authToken });
   } catch (error) {
