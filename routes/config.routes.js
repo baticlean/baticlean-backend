42f3c9fe26e8b96f5f88e3569849f459bcc2c933
baticlean <baticlean225@gmail.com>
2025-08-09 01:41:47 +0000

    test de configroute

--- routes/config.routes.js
+++ routes/config.routes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get('/forgot-password-status', (req, res) => {
     // Lit la variable d'environnement et la convertit en booléen
-    const isMaintenance = process.env.FORGOT_PASSWORD_MAINTENANCE_MODE === 'true';
+    const isMaintenance = process.env.FORGOT_PASSWORD_MAINTENANCE_MODE === 'false';
 
     // Renvoie le statut au frontend
     res.status(200).json({
